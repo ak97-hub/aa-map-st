ed665d1a5200785f2902be4b34bfb38208c60ccb
ak97-hub <79121728+ak97-hub@users.noreply.github.com>
2024-08-12 19:01:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ st.write("Hi I'm Chabely, a Certified Anesthesiologist Assistant! Check out my [
 
 st.markdown("---")
 st.subheader("Updates: :tada: :tada: :tada:")
+st.markdown("- Kansas City University announces plans for anesthesiologist assistant program beginning in January 2026")
 st.markdown("- Washington 2024: Veterans Affairs ->> Full Practice")
 st.markdown("- Nevada 2023: Veterans Affairs ->> Full Practice; bill signed 2023, effective 1/1/24")
 st.markdown("- Ongoing legislation: PA, AZ, TN, IL")
