06c1ef85cd31991d4c4def76dd442bdff085a38f
ak97-hub <79121728+ak97-hub@users.noreply.github.com>
2024-08-12 19:08:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.markdown("- Kansas City University announces plans for anesthesiologist assis
 st.markdown("- Washington 2024: Veterans Affairs ->> Full Practice")
 st.markdown("- Nevada 2023: Veterans Affairs ->> Full Practice; bill signed 2023, effective 1/1/24")
 st.markdown("- Ongoing legislation: PA, AZ, TN, IL")
-st.markdown("NOTES:")
+st.subheader("NOTES:")
 st.markdown("- Kentucky- requires PA licensing in addition to AA licensing")
 st.markdown("- Click circle on map to see school name")
 
